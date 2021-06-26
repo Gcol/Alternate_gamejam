d5f989e2177963950fe8239c43804b5efd574141
Yann Roirand <y.roirand@gmail.com>
2021-06-26 13:31:16 +0200

    toy 0.1
    move the circle with arrowkeys - no collisions

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    public InputAction dPadMvt;
+    public CharacterController controller;
+    public float templateSpeed = 3.14f;
+
+    void OnEnable()
+    {
+        dPadMvt.Enable();
+    }
+
+    void OnDisable()
+    {
+        dPadMvt.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = dPadMvt.ReadValue<Vector2>();
+        Debug.Log(dPadMvt.ReadValue<Vector2>().ToString());
+        controller.Move(inputVector * Time.deltaTime * templateSpeed);
+    }
+}
